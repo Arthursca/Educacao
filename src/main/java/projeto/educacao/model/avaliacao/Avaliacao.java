e94aef36f195dcafe3265fc5384a03e6f7e8ff9b
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-07-21 17:23:40 -0300

    Create Avaliacao

---
+++ src/main/java/projeto/educacao/model/avaliacao/Avaliacao.java
@@ -0,0 +1,44 @@
+package projeto.educacao.model.avaliacao;
+
+import projeto.educacao.model.afd.AFD;
+import projeto.educacao.model.atu.ATU;
+import projeto.educacao.model.dsu.DSU;
+import projeto.educacao.model.had.HAD;
+import projeto.educacao.model.icg.ICG;
+import projeto.educacao.model.ied.IED;
+import projeto.educacao.model.ird.IRD;
+import projeto.educacao.model.municipio.Municipio;
+import projeto.educacao.model.tdi.TDI;
+import projeto.educacao.model.tnr.TNR;
+import projeto.educacao.model.txr.TXR;
+
+import java.util.List;
+
+public class Avaliacao {
+
+    private Long id;
+
+    private Long ano;
+
+    private AFD afd;
+
+    private ATU atu;
+
+    private DSU dsu;
+
+    private HAD had;
+
+    private ICG icg;
+
+    private IED ied;
+
+    private IRD ird;
+
+    private TDI tdi;
+
+    private TNR tnr;
+
+    private TXR txr;
+
+    private Municipio municipio;
+}
