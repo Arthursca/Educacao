3036ab7e5a6f65c08045ff3c7ae20bf087511c1d
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-07-26 10:31:22 -0300

    feat: create municipio model

--- src/main/java/projeto/educacao/model/municipio/DTO/MunicipioDto.java
+++ src/main/java/projeto/educacao/model/municipio/DTO/MunicipioDto.java
@@ -1,4 +1,76 @@
 package projeto.educacao.model.municipio.DTO;
 
-public class MunicipioDto {
+import javax.persistence.Column;
+
+public class MunicipioDTO {
+
+    private Long id;
+
+    private String regiao;
+
+    private String UF;
+
+    private Long codMunicipio;
+
+    private String nomeMunicipio;
+
+    private String localizacao;
+
+    private String dependenciaAdm;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public Long getCodMunicipio() {
+        return codMunicipio;
+    }
+
+    public void setCodMunicipio(Long codMunicipio) {
+        this.codMunicipio = codMunicipio;
+    }
+
+    public String getNomeMunicipio() {
+        return nomeMunicipio;
+    }
+
+    public void setNomeMunicipio(String nomeMunicipio) {
+        this.nomeMunicipio = nomeMunicipio;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public String getDependenciaAdm() {
+        return dependenciaAdm;
+    }
+
+    public void setDependenciaAdm(String dependenciaAdm) {
+        this.dependenciaAdm = dependenciaAdm;
+    }
 }
--- src/main/java/projeto/educacao/model/municipio/Municipio.java
+++ src/main/java/projeto/educacao/model/municipio/Municipio.java
@@ -1,24 +1,89 @@
 package projeto.educacao.model.municipio;
 
-import projeto.educacao.model.avaliacao.Avaliacao;
 
-import java.util.List;
+import javax.persistence.*;
 
+
+@Entity
+@Table(name = "tb_municipio")
 public class Municipio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "regiao")
     private String regiao;
 
+    @Column(name = "uf")
     private String UF;
 
+    @Column(name = "cod_municipio")
     private Long codMunicipio;
 
+    @Column(name = "nome_municipio")
     private String nomeMunicipio;
 
+    @Column(name = "localizacao")
     private String localizacao;
 
+    @Column(name = "dependencia_adm")
     private String dependenciaAdm;
 
-    private List<Avaliacao> avaliacao;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public Long getCodMunicipio() {
+        return codMunicipio;
+    }
+
+    public void setCodMunicipio(Long codMunicipio) {
+        this.codMunicipio = codMunicipio;
+    }
+
+    public String getNomeMunicipio() {
+        return nomeMunicipio;
+    }
+
+    public void setNomeMunicipio(String nomeMunicipio) {
+        this.nomeMunicipio = nomeMunicipio;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public String getDependenciaAdm() {
+        return dependenciaAdm;
+    }
+
+    public void setDependenciaAdm(String dependenciaAdm) {
+        this.dependenciaAdm = dependenciaAdm;
+    }
 }
